39f6f2b7a0940f490b82f1cdd08d59d694475dac
dabdelwahed <dabdelwahed@mobelite.fr>
2022-05-12 10:26:42 +0100

    Initial Commit

---
+++ LoginFormSwiftUI/LoginFormSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormSwiftUIApp.swift
+//  LoginFormSwiftUI
+//
+//  Created by Dorra Ben Abdelwahed on 12/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginFormSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
